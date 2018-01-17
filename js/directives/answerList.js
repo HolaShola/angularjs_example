9a0a52820029a0e5c5ee08e4beb57c60bc5d3e3b
Yauheni_Sholakhau <Yauheni_Sholakhau@epam.com>
2018-01-17 17:18:17 +0300

    add simple answerList directive

---
+++ js/directives/answerList.js
@@ -0,0 +1,16 @@
+myApp.directive("answerList", function () {
+    return function (scope, element, attrs) {
+        console.log(scope);
+        console.log(scope.question);
+        var data = scope[attrs["answerList"]];
+        if (angular.isArray(data.answers)) {
+            var ulElem = angular.element("<ul>");
+            element.append(ulElem);
+            for (var i = 0; i < data.answers.length; i++) {
+                var liElem = angular.element('<li>');
+                liElem.append(angular.element('<p>').text(data.answers[i].text));
+                ulElem.append(liElem);
+            }
+        }
+    }
+});
